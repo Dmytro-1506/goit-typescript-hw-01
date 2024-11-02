b671ac31c129c05ddc1943367080d1fadbe18343
Dmytro-1506 <kyunlik@gmail.com>
2024-11-02 22:31:24 +0100

    work with lesson 2

--- ts_courses/lesson-2.ts
+++ ts_courses/lesson-2.ts
@@ -177,3 +177,74 @@ console.log(studentName); // 'John'
 
 // let studentAddress = getProperty(student, 'address');
 // console.log(studentAddress); // undefinde
+
+class DataStorage<T> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Hello');
+textStorage.addItem('World');
+console.log(textStorage.getItems()); // ['Hello', 'World']
+// textStorage.addItem(1); // Error: Argument of type 'number' is not assignable to parameter of type 'string'
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(1);
+numberStorage.addItem(2);
+console.log(numberStorage.getItems()); // [1, 2]
+// numberStorage.addItem('TEXT'); // Error: Argument of type 'number' is not assignable to parameter of type 'number'
+
+class KeyValuePair<TKey, TValue> {
+  constructor(private key: TKey, private value: TValue) {}
+
+  getKey(): TKey {
+    return this.key;
+  }
+
+  getValue(): TValue {
+    return this.value;
+  }
+}
+
+const pair1 = new KeyValuePair('name', 'Alice');
+console.log(pair1.getKey()); // 'name'
+console.log(pair1.getValue()); // 'Alice'
+
+const pair2 = new KeyValuePair(1, true);
+console.log(pair2.getKey()); // 1
+console.log(pair2.getValue()); // true
+
+
+// ----- Utility Types -----
+
+type User12 = {
+  id: number;
+  name: string;
+  email: string;
+  registered: boolean;
+};
+
+function createUser(data: Partial<User12>): User12 {
+  // Деякі значення за замовчуванням:
+  const defaultUser: User12 = {
+    id: Date.now(),
+    name: '',
+    email: '',
+    registered: false,
+  };
+
+  // З'єднуємо дані користувача та значення за замовчуванням
+  return { ...defaultUser, ...data };
+}
+
+const newUser = createUser({ name: 'Alice', email: 'alice@example.com' });
+
+console.log(newUser);
